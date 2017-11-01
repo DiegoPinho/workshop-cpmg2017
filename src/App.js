97e7b6f4e5ec0dc3bf0e7d8ae3d16898a544ac35
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2017-11-01 13:29:41 -0200

    estrutura inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react';
 import {Grid, Row, Col} from 'react-bootstrap';
 
-import Navio from './Navio';
-import Controles from './Controles';
-
-import './App.css';
-
 class App extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      'clima': 'sol'
-    }
-
-    this.mudaClima = this.mudaClima.bind(this);
-  }
-
-  mudaClima(clima) {
-    this.setState({clima});
-  }
   
   render() {
     return (
-      <Grid>
-        <Row>
-          <Col md={6}><Navio clima={this.state.clima}/></Col>
-          <Col md={6}><Controles callback={this.mudaClima} /></Col>
-        </Row>
-      </Grid>
+     <div>Campus Party!</div>
     );
   }
 }
